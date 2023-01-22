9ecd83650e469b23229b0371d95f8a9890c4b662
tolok oleksandr <tolok.oleksandr@gmail.com>
2023-01-22 13:06:53 +0100

    Created file quizes.json with data inside

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,19 @@
     </header>
 
     <!-- Cards -->
-    <div class="options-container"></div>
+    <div class="options-container">
+      <div class="card">
+        <img
+          src="https://cdn5-ss3.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg"
+          alt="Math"
+        />
+
+        <div class="card-text">
+          <h2>Math</h2>
+          <p>15 questions</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,7 +28,6 @@
 .container {
   max-width: 1000px;
   margin: 0 auto;
-  display: flex;
 }
 
 header {
@@ -29,6 +40,7 @@ header {
 header h1 {
   font-weight: bold;
   margin-right: 30px;
+  color: royalblue;
 }
 
 header input {
@@ -36,5 +48,36 @@ header input {
   background-color: rgba(128, 128, 128, 0.1);
   padding: 10px;
   border-radius: 5px;
+}
+
+.options-container {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 40px;
+}
+
+/* Card */
+.card {
+  width: 310px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 35px;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.card img {
+  width: 100%;
+  height: 190px;
+  margin: 0;
+}
+
+.card .card-text {
+  padding: 0 5px;
+}
+
+.card .card-text {
+  font-weight: bold;
 }
 </style>
