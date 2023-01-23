900d08223ee68b529c2507f523b71949647e9862
tolok oleksandr <tolok.oleksandr@gmail.com>
2023-01-23 10:18:11 +0100

    Using Card.vue in App.js

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,12 @@ import q from "./data/quizes.json";
 
 // Importing ref for to bind the  state
 import { ref, watch } from "vue";
+import Card from "./components/Card.vue";
 
 const quizes = ref(q); // all data is here
 const search = ref(""); // input
 
-// Every time listening on change in input field
+// Every time listening on change in input field, passing through each quiz and keep if input have the same latter
 watch(search, () => {
   quizes.value = q.filter((quiz) =>
     quiz.name.toLowerCase().includes(search.value.toLowerCase())
@@ -30,14 +31,15 @@ watch(search, () => {
 
     <!-- Cards -->
     <div class="options-container">
-      <div v-for="quiz in quizes" :key="quiz.id" class="card">
+      <Card />
+      <!-- <div v-for="quiz in quizes" :key="quiz.id" class="card">
         <img :src="quiz.img" alt="" />
 
         <div class="card-text">
           <h2>{{ quiz.name }}</h2>
           <p>{{ quiz.questions.length }} questions</p>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -75,27 +77,4 @@ header input {
 }
 
 /* Card */
-.card {
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer;
-}
-
-.card img {
-  width: 100%;
-  height: 190px;
-  margin: 0;
-}
-
-.card .card-text {
-  padding: 0 5px;
-}
-
-.card .card-text {
-  font-weight: bold;
-}
 </style>
