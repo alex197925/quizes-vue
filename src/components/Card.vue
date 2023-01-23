a0909328d30e837b9b3871de7d93e4e514265022
tolok oleksandr <tolok.oleksandr@gmail.com>
2023-01-23 10:17:09 +0100

    Created Card component, transfer HTML and Css in to it

---
+++ src/components/Card.vue
@@ -0,0 +1,40 @@
+<!-- @format -->
+
+<template>
+  <div class="card">
+    <img
+      src="https://img.freepik.com/premium-vector/realistic-math-chalkboard-background_23-2148154055.jpg?w=2000"
+      alt=""
+    />
+    <div class="card-text">
+      <h2>Math</h2>
+      <p>3 questions</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  width: 310px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 35px;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.card img {
+  width: 100%;
+  height: 190px;
+  margin: 0;
+}
+
+.card .card-text {
+  padding: 0 5px;
+}
+
+.card .card-text {
+  font-weight: bold;
+}
+</style>
