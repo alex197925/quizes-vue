e857c508440658c63836c30b2a5a640901fa3624
tolok oleksandr <tolok.oleksandr@gmail.com>
2023-02-02 13:16:54 +0100

    Question bar, Percentage bar working.Using computed

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,7 +1,58 @@
 <!-- @format -->
+<script setup>
+import Question from "../components/Question.vue";
+import QuizHeader from "../components/QuizHeader.vue";
+import { useRoute } from "vue-router";
+import { ref, watch, computed } from "vue";
+import quizes from "../data/quizes.json";
+
+const route = useRoute();
+// console.log(route.params);
+
+const quizId = parseInt(route.params.id);
+
+const quiz = quizes.find((q) => q.id === quizId);
+
+// Data - state
+const currentQuestionIndex = ref(0);
+
+// const questionStatus = ref(
+//   `${currentQuestionIndex.value}/${quiz.questions.length}`
+// );
+
+// Updating value of questions
+// watch(
+//   () => currentQuestionIndex.value,
+//   () => {
+//     questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`;
+//     console.log("currentQuestionIndex has changed");
+//   }
+// );
+
+const questionStatus = computed(
+  () => `${currentQuestionIndex.value}/${quiz.questions.length}`
+);
+
+// Add percentage to the  status bar
+const barPercentage = computed(
+  () => `${(currentQuestionIndex.value / quiz.questions.length) * 100}%`
+);
+</script>
 
 <template>
+  <!-- {{ barPercentage }} -->
+  <!-- {{ quizId }} -->
+  <!-- {{ currentQuestionIndex }} -->
+  <!-- {{ quizes }} -->
   <div>
-    <h1>Quiz Page</h1>
+    <QuizHeader
+      :questionStatus="questionStatus"
+      :barPercentage="barPercentage"
+    />
+
+    <div>
+      <Question :question="quiz.questions[currentQuestionIndex]" />
+    </div>
+    <button @click="currentQuestionIndex++">Click</button>
   </div>
 </template>
